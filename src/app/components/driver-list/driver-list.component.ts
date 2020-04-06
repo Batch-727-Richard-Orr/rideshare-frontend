19d534ee9762337c19c1a410c5dff2897defe105
Wideman <wideman.christopher.roy@gmail.com>
2020-04-06 15:35:13 -0400

    modularizing

--- src/app/components/driver-list/driver-list.component.ts
+++ src/app/components/driver-list/driver-list.component.ts
@@ -56,7 +56,6 @@ export class DriverListComponent implements OnInit {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-
   searchDriver() {
     //call service search algorithm ()
     console.log("Searching for Drivers");
@@ -80,8 +79,6 @@ export class DriverListComponent implements OnInit {
           )
           this.drivers.push(driver);
         });
-
-        this.emptyDriversList();
 
         this.emptyDriversList();
 
@@ -208,33 +205,61 @@ export class DriverListComponent implements OnInit {
 
 
     });
-    // console.log (list);
-    // console.log(distance);
-    // console.log(time);
 
     this.time = time;
     this.distance = distance;
     this.driversList = list;
 
+    console.log('------------------driversList----------------')
+    console.log(this.driversList);
 
-    this.time = time;
-    this.distance = distance;
-    this.driversList = list;
-
-
-
-    // });
-
+    //this.fillTable();
 
   }
 
+  fillTable(){
+    var outputDiv = document.getElementById('output');
+    for(let i=0;i<this.driversList.length;i++){
+      //console.log('--------------here-----------------')
+      outputDiv.innerHTML += `<tr><td class="col">${this.driversList[i].name}</td>
+          <td class="col">${this.distance[i].text}</td>
+          <td class="col">${this.time[i].text}</td>
+          <td class="col">${this.driversList[i].car ? this.driversList[i].car.seats : 0}</td>
+          <td class="col">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${this.driversList[i].id}"> View</button>
+            <div class="col-lg-5">
+            <div class="modal " id="exampleModalCentered${this.driversList[i].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content ">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                      <h1 style="color: #f16a2c;">${this.driversList[i].name}</h1>
+                      <span class="text-muted">Email: </span><h3>${this.driversList[i].email}</h3>
+                      <span class="text-muted">Phone: </span><h3>${this.driversList[i].phone}</h3>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+          <div class="col-lg-6">
+              <div #maps id="gmap" class="img-responsive"></div>
+          </div>
+        </td></tr>`;
+    }
+  }
+
+
   emptyDriversList() {
-
-
     var outputDiv = document.getElementById('output');
     outputDiv.innerHTML = ``;
-
-
   }
 
   sortByName() {
@@ -265,51 +290,17 @@ export class DriverListComponent implements OnInit {
     let tempDriverList: Array<any> = [];
 
     let mark = 0;
-    var outputDiv = document.getElementById('output');
     sortDr.forEach(sDr => {
       tempDistance.push(this.distance[index[mark]]);
       tempTime.push(this.time[index[mark]]);
       tempDriverList.push(this.driversList[index[mark]]);
-
-      outputDiv.innerHTML += `<tr><td class="col">${sDr}</td>
-        <td class="col">${this.distance[index[mark]].text}</td>
-        <td class="col">${this.time[index[mark]].text}</td>
-        <td class="col">${this.driversList[index[mark]].car ? this.driversList[index[mark]].car.seats : 0}</td>
-        <td class="col">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${this.driversList[index[mark]].id}"> View</button>
-          <div class="col-lg-5">
-          <div class="modal " id="exampleModalCentered${this.driversList[index[mark]].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content ">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                    <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                    <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-                    <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-              </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div #maps id="gmap" class="img-responsive"></div>
-        </div>
-      </td></tr>`;
       mark++
-
     })
 
     this.distance = tempDistance;
     this.time = tempTime;
     this.driversList = tempDriverList;
+    this.fillTable();
   }
 
   sortByDistance() {
@@ -343,55 +334,14 @@ export class DriverListComponent implements OnInit {
       tempDistance.push(this.distance[index[mark]]);
       tempTime.push(this.time[index[mark]]);
       tempDriverList.push(this.driversList[index[mark]]);
-
-      outputDiv.innerHTML += `<tr><td class="col">${this.driversList[index[mark]].name}</td>
-        <td class="col">${this.distance[index[mark]].text}</td>
-        <td class="col">${this.time[index[mark]].text}</td>
-        <td class="col">${this.driversList[index[mark]].car ? this.driversList[index[mark]].car.seats : 0}</td>
-        <td class="col">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${this.driversList[index[mark]].id}"> View</button>
-          <div class="col-lg-5">
-          <div class="modal" id="exampleModalCentered${this.driversList[index[mark]].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                    <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                    <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-                    <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-              </div>
-                  <div class="modal-body">
-                  <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                  <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-		              <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div #maps id="gmap" class="img-responsive"></div>
-        </div>
-      </td></tr>`;
-      mark++
-
+      mark++;
     })
 
     this.distance = tempDistance;
     this.time = tempTime;
     this.driversList = tempDriverList;
+
+    this.fillTable();
   }
 
   sortByTime() {
@@ -425,55 +375,14 @@ export class DriverListComponent implements OnInit {
       tempDistance.push(this.distance[index[mark]]);
       tempTime.push(this.time[index[mark]]);
       tempDriverList.push(this.driversList[index[mark]]);
-
-      outputDiv.innerHTML += `<tr><td class="col">${this.driversList[index[mark]].name}</td>
-        <td class="col">${this.distance[index[mark]].text}</td>
-        <td class="col">${this.time[index[mark]].text}</td>
-        <td class="col">${this.driversList[index[mark]].car ? this.driversList[index[mark]].car.seats : 0}</td>
-        <td class="col">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${this.driversList[index[mark]].id}"> View</button>
-          <div class="col-lg-5">
-          <div class="modal" id="exampleModalCentered${this.driversList[index[mark]].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                    <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                    <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-                    <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>                
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-              </div>
-                  <div class="modal-body">
-                  <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                  <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-		              <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div #maps id="gmap" class="img-responsive"></div>
-        </div>
-      </td></tr>`;
-      mark++
-
+      mark++;
     })
 
     this.distance = tempDistance;
     this.time = tempTime;
     this.driversList = tempDriverList;
+
+    this.fillTable();
   }
 
   sortBySeats() {
@@ -508,55 +417,14 @@ export class DriverListComponent implements OnInit {
       tempDistance.push(this.distance[index[mark]]);
       tempTime.push(this.time[index[mark]]);
       tempDriverList.push(this.driversList[index[mark]]);
-
-      outputDiv.innerHTML += `<tr><td class="col">${this.driversList[index[mark]].name}</td>
-        <td class="col">${this.distance[index[mark]].text}</td>
-        <td class="col">${this.time[index[mark]].text}</td>
-        <td class="col">${this.driversList[index[mark]].car ? this.driversList[index[mark]].car.seats : 0}</td>
-        <td class="col">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${this.driversList[index[mark]].id}"> View</button>
-          <div class="col-lg-5">
-          <div class="modal" id="exampleModalCentered${this.driversList[index[mark]].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                    <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                    <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-                    <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>                
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-              </div>
-                  <div class="modal-body">
-                  <h1 style="color: #f16a2c;">${this.driversList[index[mark]].name}</h1>
-                  <span class="text-muted">Email: </span><h3>${this.driversList[index[mark]].email}</h3>
-		              <span class="text-muted">Phone: </span><h3>${this.driversList[index[mark]].phone}</h3>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div #maps id="gmap" class="img-responsive"></div>
-        </div>
-      </td></tr>`;
-      mark++
-
+      mark++;
     })
     
     this.distance = tempDistance;
     this.time = tempTime;
     this.driversList = tempDriverList;
+
+    this.fillTable();
   }
 
 }
